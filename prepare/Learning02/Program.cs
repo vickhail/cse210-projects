e449df4d7dbda04e0ebddc8fcddc577c570ad0e1
vickhail <escobarcote1580@gmail.com>
2024-05-09 17:35:32 -0400

    prove-w2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Graphic Designer";
+        job2._company = "Pixar";
+        job2._startYear = 2019;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "MJ E3";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
